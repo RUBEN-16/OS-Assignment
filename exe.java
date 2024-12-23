1d34e95fc423508df18cdcae5986b5cdbf92c3fb
Ruben <rubeneswaranramalingam@gmail.com>
2024-12-24 05:08:55 +0800

    .

--- exe.java
+++
@@ -1,7 +0,0 @@
-public class exe {
-    public static void main(String[] args) {
-        int time = 9;
-        System.out.println(String.valueOf(time));
-        System.out.println(((Object) time).getClass().getSimpleName());
-    }
-}
